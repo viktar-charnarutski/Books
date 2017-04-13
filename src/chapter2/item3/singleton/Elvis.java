41c4eeb65bb80879a5f44b69ffe52720148a70c5
viktar_charnarutski <v.charnarutski@gmail.com>
2017-04-12 22:10:26 -0700

    singleton - single-element enum

---
+++ src/chapter2/item3/singleton/Elvis.java
@@ -0,0 +1,20 @@
+package chapter2.item3.singleton;
+
+/**
+ * Single-element enum type is the best way to implement a singleton.
+ * <p>
+ * Pros:
+ * <ul>
+ * <li>serialization machinery</li>
+ * <li>ironclad guarantee against multiple instantiations (reflection attack)</li>
+ * </ul>
+ * </p>
+ */
+public enum Elvis {
+
+    INSTANCE;
+
+    public void leaveTheBuilding() {
+
+    }
+}
