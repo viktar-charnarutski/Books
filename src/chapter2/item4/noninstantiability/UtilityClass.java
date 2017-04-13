7a18c6fe972e683b6e7102a2c0de2eef11f05332
viktar_charnarutski <v.charnarutski@gmail.com>
2017-04-12 22:27:33 -0700

    Enforce nonistantiability with a private constructor.

---
+++ src/chapter2/item4/noninstantiability/UtilityClass.java
@@ -0,0 +1,13 @@
+package chapter2.item4.noninstantiability;
+
+/**
+ * Noninstantiable utility class
+ */
+public class UtilityClass {
+    // Suppress default constructor for noninstantiability
+    private UtilityClass() {
+        // provides insurance in case the constructor is accidentally invoked with the class
+        throw new AssertionError();
+    }
+    // Remainder omitted
+}
