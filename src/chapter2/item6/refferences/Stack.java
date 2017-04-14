86fd388e2c28b3e2a18c239c2ad01d30846aec57
viktar_charnarutski <v.charnarutski@gmail.com>
2017-04-13 23:06:04 -0700

    simple stack implementation

---
+++ src/chapter2/item6/refferences/Stack.java
@@ -0,0 +1,40 @@
+package chapter2.item6.refferences;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+/**
+ * Simple stack implementation.
+ *
+ * A memory leak could be by {@code pop} method if poped object, which could has an obsolete reference, was not
+ * nullified.
+ */
+public class Stack {
+    private Object[] stack;
+    private int size = 0;
+    private static final int INITIAL_CAPACITY = 16;
+
+    public Stack() {
+        stack = new Object[INITIAL_CAPACITY];
+    }
+
+    public void push(Object obj) {
+        ensureCapacity();
+        stack[size] = obj;
+    }
+
+    public Object pop() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        Object obj = stack[--size];
+        stack[size] = null; // eliminate obsolete reference
+        return obj;
+    }
+
+    private void ensureCapacity() {
+        if (size == INITIAL_CAPACITY) {
+            stack = Arrays.copyOf(stack, size * 2 + 1);
+        }
+    }
+}
