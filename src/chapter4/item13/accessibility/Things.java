1e55ee98e0f7fd3a93ee74bff0a4ff8efe9de071
viktar_charnarutski <v.charnarutski@gmail.com>
2017-04-20 23:08:15 -0700

    minimize the accessibility of classes and members

---
+++ src/chapter4/item13/accessibility/Things.java
@@ -0,0 +1,18 @@
+package chapter4.item13.accessibility;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Private array and public immutable list.
+ * <p>
+ * It's wrong for a class to have a public static final array field, or an accessor that returns such a field.
+ */
+public class Things {
+
+    private static final String[] PRIVATE_IP_ADDRESSES = {"127.0.0.1", "127.0.0.2", "127.0.0.3"};
+
+    public static final List<String> IP_ADDRESSES = Collections.unmodifiableList(Arrays.asList(PRIVATE_IP_ADDRESSES));
+
+}
