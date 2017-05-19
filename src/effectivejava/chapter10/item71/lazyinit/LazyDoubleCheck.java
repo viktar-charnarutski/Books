675727ea9ee6d5ea2e710381d1686712b3fbf099
viktar_charnarutski <v.charnarutski@gmail.com>
2017-05-18 23:01:39 -0700

    Double-check idiom for lazy initialization of instance fields.

---
+++ src/effectivejava/chapter10/item71/lazyinit/LazyDoubleCheck.java
@@ -0,0 +1,25 @@
+package effectivejava.chapter10.item71.lazyinit;
+
+/**
+ * Double-check idiom for lazy initialization of instance fields.
+ * Helps to avoid a cost of locking if the field has been already initialized.
+ */
+public class LazyDoubleCheck {
+    private volatile FieldType field;
+
+    FieldType getField() {
+        FieldType result = field;
+        if (result == null) { // first check without locking
+            synchronized (this) {
+                if (field == null) { // second check with locking
+                    result = field = calculateFieldValue();
+                }
+            }
+        }
+        return result;
+    }
+
+    private FieldType calculateFieldValue() {
+        return new FieldType();
+    }
+}
