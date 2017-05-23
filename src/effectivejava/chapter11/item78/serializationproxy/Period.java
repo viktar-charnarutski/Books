b766a5ece43d604b6c15f913f2029be18a1bedd2
viktar_charnarutski <v.charnarutski@gmail.com>
2017-05-22 20:56:18 -0700

    Adding parameters' check to the class's constructor. Adding accessor methods.

--- src/effectivejava/chapter11/item78/serializationproxy/Period.java
+++ src/effectivejava/chapter11/item78/serializationproxy/Period.java
@@ -3,7 +3,7 @@ package effectivejava.chapter11.item78.serializationproxy;
 import java.io.InvalidObjectException;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
-import java.util.Date;
+import java.sql.Date;
 
 /**
  * Serializable class with Serialization proxy pattern instead of serialized instances.
@@ -12,11 +12,25 @@ public class Period implements Serializable {
     private final Date start;
     private final Date end;
 
+    // defensive copies are made before checking the validity of the parameters, and the validity check is performed on
+    // copies rather than on the originals
     public Period(Date start, Date end) {
-        this.start = start;
-        this.end = end;
+        this.start = new java.sql.Date(start.getTime());
+        this.end = new java.sql.Date(end.getTime());
+        if (this.start.after(this.end)) throw new IllegalArgumentException("Start is after end");
     }
 
+    // return defensive copies of mutable internal fields
+    public Date start() {
+        return new Date(start.getTime());
+    }
+
+    // return defensive copies of mutable internal fields
+    public Date end() {
+        return new Date(end.getTime());
+    }
+
+    // Serialization proxy pattern
     private static class SerializableProxy implements Serializable {
         private final Date start;
         private final Date end;
